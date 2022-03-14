31f55329dc954eaff3b33a9bf06f061dae9c51a4
plinh75 <nguyetnemm2001@gmail.com>
2022-03-14 09:50:10 +0700

    update

--- middlewares/checkAuth.js
+++
@@ -1,9 +0,0 @@
-export const checkAuth = (req, res, next) => {
-    const status = true;
-    if(status){
-        console.log("b1")
-        next();
-    } else {
-        console.log("false");
-    }
-}
