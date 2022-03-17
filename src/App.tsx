20dda75368bad722836780eb5e4b9cda381077dc
plinh75 <nguyetnemm2001@gmail.com>
2022-03-17 09:56:43 +0700

    tao route admin, web

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import {Navigate, NavLink, Routes, Route} from 'react-router-dom'
 import ShowInfo from './components/ShowInfo'
 
 import type {Product} from './types/product'
+
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
+import DashboardPage from './pages/DashboardPage'
+import AdminLayout from './pages/layouts/AdminLayout'
+import WebsiteLayout from './pages/layouts/WebsiteLayout'
+import ProductManager from './pages/ProductManager'
 
 function App() {
   const [count,setCount] = useState<number>(0)
@@ -13,8 +20,26 @@ function App() {
   })
   
   return <div className="App">
-    {count}<button onClick={()=> setCount(count+1)}>Click count</button>
-    <ShowInfo person={info}/>
+    <header>
+      <ul>
+        <li><NavLink to="/">Home page</NavLink></li>
+        <li><NavLink to="/product">Product page</NavLink></li>
+        <li><NavLink to="/about">About page</NavLink></li>
+      </ul>
+    </header>
+    <main>
+      <Routes>
+        <Route path="/" element={<WebsiteLayout/>}>
+          <Route index element={<HomePage/>} />
+          <Route path="product" element={<ProductPage/>} />
+        </Route>
+        <Route path="admin" element={<AdminLayout/>}>
+          <Route index element={<Navigate to="/admin/dashboard" />} />
+          <Route path="dashboard" element={<DashboardPage/>} />
+          <Route path="product" element={<ProductManager/>} />
+        </Route>
+      </Routes>
+    </main>
   </div>
 }
 
