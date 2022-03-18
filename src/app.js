61cc2a26bca28abeaa86f9c3d99000815bb6182b
plinh75 <nguyetnemm2001@gmail.com>
2022-03-18 21:39:33 +0700

    xóa nhầm file nên thêm lại :(

---
+++ src/app.js
@@ -0,0 +1,34 @@
+// const express = require('express');
+import express from "express";
+import cors from 'cors';// cấp quyền truy cập
+import morgan from "morgan";
+import mongoose from "mongoose";
+
+import productRouter from '../routers/product';
+import categoryRouter from '../routers/category';
+// import newRouter from '../routes/new';
+import userRouter from '../routers/user';
+//morgan dùng để thông báo khi thực hiện get hay post ... dùng npm i morgan
+
+
+const app = express();
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(express.json());// phải đứng trc để chuyển kiểu 
+// app.use(productRouter);
+// app.use(newRouter);
+app.use(userRouter);
+app.use(productRouter)
+app.use(categoryRouter)
+
+// connect databse
+mongoose.connect('mongodb://localhost:27017/web16309')
+.then(() => console.log("Kết nối db thành công"))
+.catch((error) => console.log(error));
+
+
+//connection
+ const PORT = 3001;
+ app.listen(PORT, () => {
+     console.log("Server is running port", PORT);
+ })
